4192a4f70bf397904dd7b83dc83f01f4811a9574
Thomas Lamprecht <info@gamer-source.org>
2011-03-31 20:16:40 +0200

    Added READNE abd License (GPLv2)

--- game_of_life.h
+++ game_of_life.h
@@ -1,13 +1,22 @@
-// leben = 1
-// tot = -1
+/*
+This is (part of) a simple implementation of the famous cellular automaton "Game of Life" from John Horton Conway
+Copyright (C) 2011  Thomas Lamprecht
 
-// Besetzt_
-	// Überleben = 2-3 nachbaren
-	// Stirbt 0-1|4-8
-// Unbesetzt
-	// Neues Leben = 3 Nachbaren
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+or look at http://www.gnu.org/licenses/gpl-2.0-standalone.html
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
